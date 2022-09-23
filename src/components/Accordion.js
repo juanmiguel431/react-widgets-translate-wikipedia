655aceb0d0a46bb68c31f4d107b988c23fff058f
juanmiguel431 <juanmiguel431@gmail.com>
2022-09-22 23:55:41 -0400

    Using React.Fragment

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,9 @@
+import React from "react";
+
 const Accordion = ({items}) => {
 
   const renderedItems = items.map(item => {
-    return <div key={item.title}>
+    return <React.Fragment key={item.title}>
       <div className="title active">
         <i className="dropdown icon"></i>
         {item.title}
@@ -9,7 +11,7 @@ const Accordion = ({items}) => {
       <div className="content active">
         <p>{item.content}</p>
       </div>
-    </div>
+    </React.Fragment>
   });
 
   return (
