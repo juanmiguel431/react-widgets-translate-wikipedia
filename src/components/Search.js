8f0a0761282ca5965f4ce1bef362cffea2649238
juanmiguel431 <juanmiguel431@gmail.com>
2022-09-25 15:08:44 -0400

    Navigation to wikipedia added.

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,23 +2,29 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState('programming');
+  const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
 
   useEffect(() => {
     const search = async () => {
-      const response = await axios.get('/w/api.php', {
-        baseURL: 'https://en.wikipedia.org',
-        params: {
-          action: 'query',
-          list: 'search',
-          origin: '*',
-          format: 'json',
-          srsearch: term
-        }
-      });
+      let data;
+      if (term) {
+        const response = await axios.get('/w/api.php', {
+          baseURL: 'https://en.wikipedia.org',
+          params: {
+            action: 'query',
+            list: 'search',
+            origin: '*',
+            format: 'json',
+            srsearch: term
+          }
+        });
+        data = response.data.query.search
+      } else {
+        data = [];
+      }
 
-      setResults(response.data.query.search);
+      setResults(data);
     };
 
     search();
@@ -27,6 +33,14 @@ const Search = () => {
   const renderedResults = results.map(result => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Go</a>
+        </div>
         <div className="content">
           <div className="header">
             <span dangerouslySetInnerHTML={{ __html: result.title }}/>
