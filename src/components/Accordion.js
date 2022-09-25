b330e36891ccf2ce61ff4602f3ecded5f9c114f9
juanmiguel431 <juanmiguel431@gmail.com>
2022-09-24 20:15:24 -0400

    Functional components - useState

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Accordion = ({items}) => {
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
 
-  const renderedItems = items.map(item => {
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  }
+
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
     return <React.Fragment key={item.title}>
-      <div className="title active">
-        <i className="dropdown icon"></i>
+      <div
+        className={`title ${active}`}
+        onClick={_ => onTitleClick(index)}
+      >
+        <i className="dropdown icon"/>
         {item.title}
       </div>
-      <div className="content active">
+      <div className={`content ${active}`}>
         <p>{item.content}</p>
       </div>
     </React.Fragment>
